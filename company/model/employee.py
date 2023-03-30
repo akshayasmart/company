510e24a73e87b37bbbf46e127a1d62d4c5a797ba
Akshaya <idigi.akshaya@gmail.com>
2023-03-30 08:49:16 +0530

    [ADD] company added new module

--- company/model/employee.py
+++ company/model/employee.py
@@ -4,7 +4,7 @@ from odoo import  models,fields
 class Emp(models.Model):
     _name = "emp.emp"
 
-    name = fields.Char(string="First_Name", translate=True)
+    name = fields.Char(string="First_Name", translate=True, domain="[('name','like', name)]")
     last_name = fields.Char(string="Last_Name")
     age = fields.Integer(string="Age")
     emp_id = fields.Integer(string="Id")
